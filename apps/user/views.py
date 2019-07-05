36f989c6b8a0bd4230f892d8a0025bab50c53ca7
xingchenhuang <xingchenhuang@webot.co>
2019-07-05 18:44:30 +0800

    添加类视图模式

--- apps/user/views.py
+++ apps/user/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
 from django.core.urlresolvers import reverse
+from django.views.generic import View
 from user.models import User
 import re
 
@@ -92,3 +93,49 @@ def register_handle(request):
 
     # 返回应答 跳转到首页
     return redirect(reverse('goods:index'))
+
+
+class RegisterView(View):
+    # 注册页面展示
+    def get(self, request):
+        return render(request, 'register.html')
+
+    #  注册业务逻辑处理
+    def post(self, request):
+        # 注册业务逻辑处理
+        # 接收前端传过来的数据
+        username = request.POST.get('user_name')
+        password = request.POST.get('pwd')
+        email = request.POST.get('email')
+        allow = request.POST.get('allow')
+
+        # 进行数据校验
+        if not all([username, password, email]):
+            # 数据不完整
+            return render(request, 'register.html', {'errmsg': '数据不完整'})
+
+        # 邮箱校验
+        if not re.match(r'^[a-z0-9][\w.\-]*@[a-z0-9\-]+(\.[a-z]{2,5}){1,2}$', email):
+            return render(request, 'register.html', {'errmsg': '邮箱格式不正确'})
+
+        if allow != 'on':
+            return render(request, 'register.html', {'errmsg': '请同意用户协议'})
+
+        # 校验用户名是否重复
+        try:
+            user = User.objects.get(username=username)
+        except User.DoesNotExist:
+            # 用户名不存在
+            user = None
+
+        if user:
+            return render(request, 'register.html', {'errmsg': '用户名已存在'})
+
+        # 进行业务逻辑处理 直接调用django本身user处理模块
+        # user = User.objects.create_user(username, password, email)  // 参数位置错误
+        user = User.objects.create_user(username, email, password)
+        user.is_active = 0  # 用户未激活的状态
+        user.save()
+
+        # 返回应答 跳转到首页
+        return redirect(reverse('goods:index'))
